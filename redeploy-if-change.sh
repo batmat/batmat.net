41e08976d12edf0b724ee1b4fd04beaf08f02deb
Baptiste Mathus <batmat@batmat.net>
2016-01-03 07:38:21 +0100

    Always restart when stopped

--- redeploy-if-change.sh
+++ redeploy-if-change.sh
@@ -17,14 +17,14 @@ else
   echo "No image found with that name, rebuild and redeploy"
   echo "Removing exited containers"
   docker ps -aq --filter=status=exited | xargs --no-run-if-empty docker rm
-  
+
   echo "building"
   docker build -t $imageFullName .
   docker kill $containerName || echo "No container to kill named $containerName"
   docker rm $containerName || echo "No container to remove named $containerName"
 
   echo "Starting new instance"
-  docker run -d -p $containerPort:80 --name blog $imageFullName
+  docker run --restart=always -d -p $containerPort:80 --name blog $imageFullName
 
   echo "OK"
 fi
